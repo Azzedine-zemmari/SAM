5a54ff41f3a7f8198202e94a7855353c700af064
wissal <mokdadwissal@icloud.com>
2024-08-02 00:00:08 +0100

    wissal

--- Config/db.js
+++ Config/db.js
@@ -1,20 +1,14 @@
-const mysql = require('mysql');
-require('dotenv').config();
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database:process.env.DB_NAME
-});
+const mysql = require("mysql");
 
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-    return;
-  }
-  console.log('Connected to the database ');
-});
+const db = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"SAM"
+})
 
-module.exports = connection;
+db.getConnection(()=>{
+    console.log("database connected");
+})
+
+module.exports=db;
